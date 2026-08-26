13078d080848f198cd8720c4feae7d2dec688f4c
s24191 <s24191@pjwstk.edu.pl>
2026-08-26 16:54:05 +0200

    backend increase lifetime of JWT

--- shared/auth/backend.py
+++ shared/auth/backend.py
@@ -5,9 +5,15 @@ SECRET = os.getenv("JWT_SECRET")
 if not SECRET:
     raise RuntimeError("backend:JWT_SECRET is not set in .env file.")
 
+JWT_LIFETIME_SECONDS = int(
+    os.getenv(
+        "JWT_LIFETIME_SECONDS",
+        str(60 * 60 * 24 * 7),
+    )
+)
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=JWT_LIFETIME_SECONDS)
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
